a77db738e21f1feaf25ac6a31f7cd59c57b3e7b7
saka <sakamoshood@ymail.com>
2021-12-22 19:22:37 -0800

    initial commit - first contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    constructor(){
+        console.log("Yo, this my first contract and its smart too G");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("You have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
