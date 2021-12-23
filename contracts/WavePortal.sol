4acae1e399ac66c498dad52c5599ac6a2aa06cb2
saka <sakamoshood@ymail.com>
2021-12-22 22:06:51 -0800

    deloyed smart contract and working on frontend

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
+// Deploying contracts with account:  0x562Ae243F69511003495577555c569c929e2B0B5
+// Account balance of deployer:  200000000000000000
+// WavePortal address:  0xEE76E2958f92C3348893B9a0f0cB71E0907338aa
+//https://rinkeby.etherscan.io/address/0xEE76E2958f92C3348893B9a0f0cB71E0907338aa
+
 contract WavePortal {
     uint256 totalWaves;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+require('dotenv').config();
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +9,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.YOUR_ALCHEMY_API_URL,
+      accounts: [process.env.YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY],
+    },
+  },
 };
